bcae2b5f238189c80e8abf789e27ebe7cb06e9d0
SkiuteDomantas <domantas.skiute@student.howest.be>
2025-01-20 20:51:48 +0100

    Finished mobile version and implemented some javascript

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 import './style.css'
 import gsap from 'gsap';
+
+
+const hamburger = document.querySelector('.hamburger');
+const menuOverlay = document.querySelector('.menu-overlay');
+const body = document.body;
+const menuLinks = document.querySelectorAll('.menu-nav a');
+const country = document.querySelector('.country');
+
+const toggleMenu = () => {
+  menuOverlay.classList.toggle('open');
+  body.classList.toggle('no-scroll');
+}
+const closeMenu =() => {
+  menuOverlay.classList.remove('open');
+  body.classList.remove('no-scroll');
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+menuLinks.forEach(link => link.addEventListener('click', closeMenu));
+
+
