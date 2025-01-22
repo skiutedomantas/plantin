cceda3b07533307518070fabfd51e69cdfb5ae6c
SkiuteDomantas <domantas.skiute@student.howest.be>
2025-01-22 18:54:51 +0100

    Path for GitHub deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import { imagetools } from 'vite-imagetools'
 
+
+
 export default defineConfig({
+  base: '/plantin/',
   plugins: [imagetools()],
   server: {
     host: '0.0.0.0',
